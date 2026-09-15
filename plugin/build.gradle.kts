7fce0a65726ede7e1a55733ea81142dab981306a
J!nl!n <461260911@qq.com>
2026-09-15 21:02:10 +0800

    Upgrade Java and Kotlin compatibility to version 17

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -14,13 +14,13 @@ version = "0.0.6"
 
 // This matches the JDK used to build the project, and is not related to what is running on device.
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.JVM_11
+        jvmTarget = JvmTarget.JVM_17
         freeCompilerArgs.add("-Xcontext-parameters")
     }
     explicitApi()
